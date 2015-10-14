be498c20f48f0792d0dab1309a04825f8f94a019
Danielle Cadhit <dcadhit@outlook.com>
2015-10-14 01:54:02 +0000

    Update home page index and database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DcTestApp::Application.routes.draw do
+
+  root 'files#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
